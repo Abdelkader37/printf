051951bb89c2b012e5d08adb70fa0a7cd3604f16
Qrafi Abdelkader <qrafiabdelkader@gmail.com>
2023-12-04 05:00:16 -0800

    somthing gone wrong

--- s_c_d_i_foncs.c
+++ s_c_d_i_foncs.c
@@ -1,4 +1,8 @@
 #include <main.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdarg.h>
 /**
  * print_char - Prints a single character to the standard output.
  * @ch: The character to be printed.
