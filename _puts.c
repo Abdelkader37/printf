78be6f100c4e1f5bd7767d97e6ea60020f49712e
Qrafi Abdelkader <qrafiabdelkader@gmail.com>
2023-12-04 04:58:18 -0800

    somthing gone wrong

--- _puts.c
+++ _puts.c
@@ -1,4 +1,8 @@
 #include <main.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdarg.h>
 /**
 * _puts - Format and print based on the format specifier.
 * @specifier The format specifier character.
